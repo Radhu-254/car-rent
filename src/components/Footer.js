c4c0a9cab8a994dd85fee3db613d3832c465079b
Radhu-254 <radhu.kalathiya1@gmail.com>
2023-04-28 09:39:14 +0530

    final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-white py-8 text-center text-slate-500 border-t'>
       <div className='container mx-auto'>
-        &copy; 2023 Odan infotech LLP
+        &copy; 2023 Odan Infotech LLP
       </div>
     </footer>
   );
